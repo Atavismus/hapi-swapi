8b0e048a40fa4898c34d17d0e8c0b33f265c8420
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-10 10:28:32 +0200

    add constants

---
+++ config/constants.js
@@ -0,0 +1,12 @@
+export const SWAPI_URL = 'https://swapi.dev/api/';
+
+export const WOOKIE_MODE = '?format=wookiee';
+
+export const RESOURCES = [
+  'films',
+  'people',
+  'planets',
+  'species',
+  'starships',
+  'vehicles',
+];
