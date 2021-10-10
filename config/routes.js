ae28efd16dad08553d9fd02edd22447712d90052
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-10 14:43:14 +0200

    update route get

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 import { getResources } from '../api/controllers/ResourcesController.js';
 import { getSearch } from '../api/controllers/SearchController.js';
+import { getById } from '../api/controllers/GetByIdController.js';
 
 export const resources = {
   method: 'GET',
@@ -25,8 +26,13 @@ export const search = {
 
 export const get = {
   method: 'GET',
-  path: '/api/get/{resource}/{id}',
-  handler: async (request) => {
-    return 'TODO: get by resource and id';
+  path: '/api/get',
+  handler: async (request, h) => {
+    try {
+      const result = await getById(request.query);
+      return result;
+    } catch (error) {
+      return h.response(error.message).code(400);
+    }
   },
 };
