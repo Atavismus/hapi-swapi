b8cabfc3e33f5a98b09ae94a65c91d11819e4e9b
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-11 22:48:28 +0200

    update get route controller (errors and optional id)

--- api/controllers/GetByIdController.js
+++ api/controllers/GetByIdController.js
@@ -3,12 +3,23 @@ import { RESOURCES } from '../../config/constants.js';
 import Hoek from '@hapi/hoek';
 
 export const getById = async (params) => {
-  const { resource, id } = params;
-  if (!resource || !id) {
-    throw new Error('Missing parameter(s)');
+  const { resource, id = '' } = params;
+  if (!resource) {
+    throw new Error('Missing resource parameter');
   }
   if (!RESOURCES.includes(resource)) {
     throw new Error('Wrong resource parameter value');
+  }
+  if (id !== '') {
+    if (id === '0') {
+      throw new Error("Wrong resource parameter value: id 0 doesn't exist.");
+    }
+    if (parseInt(id) < 0) {
+      throw new Error('Wrong resource parameter value: id has to be positive.');
+    }
+    if (isNaN(id)) {
+      throw new Error('Wrong resource parameter value: id has to be numeric.');
+    }
   }
   try {
     const result = await getByIdService(
