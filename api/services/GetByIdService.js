6bddf403552068969d2534cdf815eb567531c3f0
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-14 07:08:27 +0200

    fix get route pagination

--- api/services/GetByIdService.js
+++ api/services/GetByIdService.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { SWAPI_URL } from '../../config/constants.js';
 
 export const getByIdService = async (resource, id, page) => {
-  page = page !== '' ? `page=${page}` : '';
+  page = page !== '' ? `?page=${page}` : '';
   try {
-    const result = await axios.get(`${SWAPI_URL}${resource}/?${page}${id}`);
+    const result = await axios.get(`${SWAPI_URL}${resource}/${page}${id}`);
     return result.data;
   } catch (error) {
     error.message = `Bad request - /api/get failed : ${error.message}`;
